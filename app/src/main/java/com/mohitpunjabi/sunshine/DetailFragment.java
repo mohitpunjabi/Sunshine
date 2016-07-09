40107e3677bd7eb0dc5ba6cf65c2369257450e59
Mohit Punjabi <me@mohitpunjabi.com>
2016-07-10 00:19:22 +0530

    Added share menu.

--- app/src/main/java/com/mohitpunjabi/sunshine/DetailFragment.java
+++ app/src/main/java/com/mohitpunjabi/sunshine/DetailFragment.java
@@ -1,10 +1,15 @@
 package com.mohitpunjabi.sunshine;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,6 +18,13 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailFragment extends Fragment {
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+    private static final String SUNSHINE_HASHTAG = "#SunshineApp";
+
+    private ShareActionProvider mShareActionProvider;
+
+    private String mForecast;
 
     public DetailFragment() {
     }
@@ -24,9 +36,11 @@ public class DetailFragment extends Fragment {
 
         Intent intent = getActivity().getIntent();
 
-        if(intent != null && intent.getExtras().containsKey(Intent.EXTRA_TEXT)) {
-            String forecast = String.valueOf(intent.getExtras().get(Intent.EXTRA_TEXT));
-            ((TextView) rootView.findViewById(R.id.forecast_data_textview)).setText(forecast);
+        if (intent != null && intent.getExtras().containsKey(Intent.EXTRA_TEXT)) {
+            mForecast = String.valueOf(intent.getExtras().get(Intent.EXTRA_TEXT));
+            ((TextView) rootView.findViewById(R.id.forecast_data_textview)).setText(mForecast);
+        } else {
+            mForecast = "";
         }
 
         return rootView;
@@ -36,6 +50,30 @@ public class DetailFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, mForecast + " " + SUNSHINE_HASHTAG);
+        return intent;
     }
 
 }
